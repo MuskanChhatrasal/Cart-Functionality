10235faecfdcc0701093ab5c6bde3cb5bf3f12ef
Muskan Chhatrasal <76219453+MuskanChhatrasal@users.noreply.github.com>
2022-03-08 06:57:23 +0530

    Initial commit

---
+++ src/itemsInCart.js
@@ -0,0 +1,26 @@
+export const itemsInCart = [
+  {
+    id: 1,
+    name: "kala chasma",
+    price: 1000,
+    quantity: 0
+  },
+  {
+    id: 2,
+    name: "laal chhadi",
+    price: 500,
+    quantity: 0
+  },
+  {
+    id: 3,
+    name: "jalebi",
+    price: 50,
+    quantity: 0
+  },
+  {
+    id: 4,
+    name: "japani joota",
+    price: 10000,
+    quantity: 0
+  }
+];
